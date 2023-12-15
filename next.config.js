afc35153d4d263fb9a19292930781fcb6b71155e
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2023-12-15 09:38:48 +0600

    Refactor: config to add firebase storage

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "http",
         hostname: "127.0.0.1",
       },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
     ],
   },
 };
