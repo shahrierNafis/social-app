bb327399887ed5057c976819d60f35b7bd0739b8
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2023-12-25 11:27:19 +0600

    Refactor: getUser to cache user data fetching

--- src/app/lib/getUser.ts
+++ src/app/lib/getUser.ts
@@ -1,5 +1,11 @@
+const cache: { [key: string]: User } = {};
 export default async function getUser(uid: string) {
-  return await (await fetch(`/profile/GET/${uid}`)).json();
+  if (cache[uid]) {
+    return cache[uid];
+  } else {
+    cache[uid] = await (await fetch(`/profile/GET/${uid}`)).json();
+    return cache[uid];
+  }
 }
 export type User = {
   photoURL: string;
