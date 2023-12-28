9b64e9486e00ea71e7757a9f3be03cc7f72bd2c5
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2023-12-28 08:59:54 +0600

    Fix: build error

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -90,7 +90,7 @@ function Page() {
           </>
         ) : (
           <>
-            {name !== user?.displayName || imgSrc ? (
+            {displayName !== user?.displayName || imgSrc ? (
               <button
                 type="button"
                 className="focus:outline-none text-black bg-yellow-700 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
