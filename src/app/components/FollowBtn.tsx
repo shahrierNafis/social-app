d6db1cbabf3d85e4bf07a8c29ee05df71707aac3
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2023-12-28 09:08:49 +0600

    Refactor: follow btn style

--- src/app/components/FollowBtn.tsx
+++ src/app/components/FollowBtn.tsx
@@ -67,7 +67,7 @@ function FollowBtn({ uid }: { uid: string }) {
         <>
           <button
             type="button"
-            className=" m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className=" m-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
             onClick={unFollow}
           >
             following
